847bc02b43c3d9277258d4ff4188e59f8b590249
glebvmx <40228716+glebvmx@users.noreply.github.com>
2019-01-15 13:42:56 +0300

    Update run.sh

--- vagrant-k8s/run.sh
+++ vagrant-k8s/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC2lB/xIP28Xr/myyQC9iPWZxKGt5xSrZ51RWbtKXZn0ZOtOo0ITXgvEgtSHZf5NID7+vDRHvWxGgcn2xGZMBBeB6VzaM/u9TXjWZPu6giI4FPYRMgCxPn/Nh3BiEHIlki0jd+NKWCp+cUVD50uzeQts7vBNw1XlppIqWueL/BRi0+aZHLhvLhxAI9jLrsYolEIhGyft5hHcQNRow9rbWPxLLBuCCYwh1dC/xoKfUVUsfktzsuwx7Da+0xTxIK3i3CDLa0r+GzrEOIjRkS0FuF1ucCg6axAncePuCLT3HeRxKXdt2jc7bh9gVfyQ/R61iaRFW2WTrP9ZNoMTeoCFGJp gleb@work" >> /root/.ssh/authorized_keys
+echo "your public key" >> /root/.ssh/authorized_keys
 cp /vagrant/kube-flannel.yml /root/
 apt update
 apt-get install python --assume-yes
